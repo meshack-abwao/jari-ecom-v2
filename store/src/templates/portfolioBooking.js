22a3c0a32e435fc348884d7bdd78349ada67288d
Meshack <meshack2141@gmail.com>
2026-01-18 17:02:37 +0300

    🎨 PBK: Center header, IG paperplane share icon, larger stars/rating

--- store/src/templates/portfolioBooking.js
+++ store/src/templates/portfolioBooking.js
@@ -39,10 +39,9 @@ export function renderPortfolioBookingTemplate(product) {
           </div>
           <div class="pbk-actions">
             <button class="pbk-action-btn" onclick="sharePbk()" title="Share">
-              <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                <path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"/>
-                <polyline points="16 6 12 2 8 6"/>
-                <line x1="12" y1="2" x2="12" y2="15"/>
+              <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5">
+                <line x1="22" y1="2" x2="11" y2="13"/>
+                <polygon points="22 2 15 22 11 13 2 9 22 2"/>
               </svg>
             </button>
             <button class="pbk-action-btn" onclick="likePbk(this)" title="Like">
