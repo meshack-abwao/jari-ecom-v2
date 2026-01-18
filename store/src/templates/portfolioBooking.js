af4ab66971488923195b3b4e709ea76bfd47aba1
Meshack <meshack2141@gmail.com>
2026-01-18 18:02:00 +0300

    🔄 PBK: Revert packages, stories, CTA to marquee commit style - full Select Package btn, IG gradient ring, centered CTA

--- store/src/templates/portfolioBooking.js
+++ store/src/templates/portfolioBooking.js
@@ -260,16 +260,12 @@ function renderPbkPackages(packages) {
         ${packages.map((pkg, i) => `
           <div class="pbk-package ${i === 0 ? 'pbk-package-featured' : ''}">
             ${i === 0 ? '<span class="pbk-package-badge">Popular</span>' : ''}
-            <div class="pbk-package-header">
-              <div class="pbk-package-info">
-                <div class="pbk-package-name">${pkg.name}</div>
-                ${pkg.duration ? `<div class="pbk-package-duration">⏱ ${pkg.duration}</div>` : ''}
-              </div>
-              <div class="pbk-package-price">KES ${parseInt(pkg.price || 0).toLocaleString()}</div>
-            </div>
+            <div class="pbk-package-name">${pkg.name}</div>
+            <div class="pbk-package-price">KES ${parseInt(pkg.price || 0).toLocaleString()}</div>
+            ${pkg.duration ? `<div class="pbk-package-duration">⏱ ${pkg.duration}</div>` : ''}
             ${pkg.description ? `<p class="pbk-package-desc">${pkg.description}</p>` : ''}
             <button class="pbk-package-btn" data-pkg-index="${i}" data-pkg-name="${pkg.name}" data-pkg-price="${pkg.price}">
-              Select
+              Select Package
             </button>
           </div>
         `).join('')}
