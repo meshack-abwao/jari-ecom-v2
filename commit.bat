18c9468b7f851254009a4113e9b396ded598229e
Meshack <meshack2141@gmail.com>
2026-01-19 06:10:39 +0300

    Phase6c: Remove old Event Landing and helper functions from render.js - 239 lines removed, now 289 lines total

--- commit.bat
+++ commit.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d C:\Users\ADMIN\Desktop\jari-ecom-v2
 git add -A
-git commit -m "Phase6b: Wire Event Landing template to dispatcher - import from isolated module"
-git log --oneline -3
+git commit -m "Phase6c: Remove old Event Landing and helper functions from render.js - 239 lines removed, now 289 lines total"
+git log --oneline -5
