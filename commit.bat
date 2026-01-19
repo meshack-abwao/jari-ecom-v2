e949584a5256a540c480cd75d24a5a865cfc5fa4
Meshack <meshack2141@gmail.com>
2026-01-19 08:05:27 +0300

    PBK: Light title font + larger share/like icons

--- commit.bat
+++ commit.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d C:\Users\ADMIN\Desktop\jari-ecom-v2
 git add -A
-git commit -m "Hero CTAs: WhatsApp + Call buttons with icons (uses store phone)"
+git commit -m "PBK: Light title font + larger share/like icons"
 git push origin main
 git log --oneline -3
