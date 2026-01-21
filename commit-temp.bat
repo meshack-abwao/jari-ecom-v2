438758fe05301fa2ba714ba9cd3a5a460b823f35
Meshack <meshack2141@gmail.com>
2026-01-22 00:55:28 +0300

    PBK-WhatsIncluded-single-col-compact-remove-grid-override

--- commit-temp.bat
+++ commit-temp.bat
@@ -1,4 +1,4 @@
 cd /d C:\Users\ADMIN\Desktop\jari-ecom-v2
 git add -A
-git commit -m "PBK-WhatsIncluded-title-inside-card-single-col-breathing-room"
+git commit -m "PBK-WhatsIncluded-single-col-compact-remove-grid-override"
 git push origin main
