d72f29b5b3f8f629fded02d8e73b5df3f7dbba3d
Meshack <meshack2141@gmail.com>
2026-01-22 01:35:38 +0300

    BKM-booking-modal-JS-JTBD-trust-flow-3-steps

--- commit-temp.bat
+++ commit-temp.bat
@@ -1,4 +1,4 @@
 cd /d C:\Users\ADMIN\Desktop\jari-ecom-v2
 git add -A
-git commit -m "docs-handover-checkout-trust-pills-complete"
+git commit -m "BKM-booking-modal-JS-JTBD-trust-flow-3-steps"
 git push origin main
