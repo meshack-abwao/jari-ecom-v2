eb8500c1a34a578b4fd8e9c23fc3d3f11cef552b
Meshack <meshack2141@gmail.com>
2026-01-22 02:00:22 +0300

    PBK-trust-section-animation-stagger-accessibility

--- commit-temp.bat
+++ commit-temp.bat
@@ -1,4 +1,4 @@
 cd /d C:\Users\ADMIN\Desktop\jari-ecom-v2
 git add -A
-git commit -m "PBK-trust-badges-JTBD-build-confidence-pills"
+git commit -m "PBK-trust-section-animation-stagger-accessibility"
 git push origin main
