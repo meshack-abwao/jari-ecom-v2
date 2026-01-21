f9c17377c54ee4928bb12305170569ea9f2669fd
Meshack <meshack2141@gmail.com>
2026-01-22 01:37:17 +0300

    BKM-booking-modal-CSS-pill-CTAs-trust-design

--- commit-temp.bat
+++ commit-temp.bat
@@ -1,4 +1,4 @@
 cd /d C:\Users\ADMIN\Desktop\jari-ecom-v2
 git add -A
-git commit -m "BKM-booking-modal-JS-JTBD-trust-flow-3-steps"
+git commit -m "BKM-booking-modal-CSS-pill-CTAs-trust-design"
 git push origin main
