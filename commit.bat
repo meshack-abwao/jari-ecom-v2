5eaf549cda4457c3938e0066af58bb87bd2e1fc1
Meshack <meshack2141@gmail.com>
2026-01-25 23:49:37 +0300

    D1.2-Dashboard-subscriptionsAPI-client-status-pricing-activate-addon

--- commit.bat
+++ commit.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d C:\Users\ADMIN\Desktop\jari-ecom-v2
 git add -A
-git commit -m "D1.1-Subscriptions-API-status-pricing-activate-addon-history"
+git commit -m "D1.2-Dashboard-subscriptionsAPI-client-status-pricing-activate-addon"
 git push origin main
 git log --oneline -5
